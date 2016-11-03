5b3dadf096e8070277ed3502afa7bf60affd5232
Jason Sims <jason@jasonsims.design>
2016-11-03 16:21:40 -0400

    Simplified the favicons

--- index.php
+++ index.php
@@ -5,30 +5,15 @@
 <title>Jason Sims Design</title>
 <meta name="description" content="Branding, Website and Print Design">
 <meta name="viewport" content="width=device-width, user-scalable=0" />
-<link rel="apple-touch-icon" sizes="57x57" href="lib/css/image/favicon/apple-touch-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="60x60" href="lib/css/image/favicon/apple-touch-icon-60x60.png">
-<link rel="apple-touch-icon" sizes="72x72" href="lib/css/image/favicon/apple-touch-icon-72x72.png">
-<link rel="apple-touch-icon" sizes="76x76" href="lib/css/image/favicon/apple-touch-icon-76x76.png">
-<link rel="apple-touch-icon" sizes="114x114" href="lib/css/image/favicon/apple-touch-icon-114x114.png">
-<link rel="apple-touch-icon" sizes="120x120" href="lib/css/image/favicon/apple-touch-icon-120x120.png">
-<link rel="apple-touch-icon" sizes="144x144" href="lib/css/image/favicon/apple-touch-icon-144x144.png">
-<link rel="apple-touch-icon" sizes="152x152" href="lib/css/image/favicon/apple-touch-icon-152x152.png">
-<link rel="apple-touch-icon" sizes="180x180" href="lib/css/image/favicon/apple-touch-icon-180x180.png">
-<link rel="icon" type="image/png" href="lib/css/image/favicon/favicon-32x32.png" sizes="32x32">
-<link rel="icon" type="image/png" href="lib/css/image/favicon/android-chrome-192x192.png" sizes="192x192">
-<link rel="icon" type="image/png" href="lib/css/image/favicon/favicon-96x96.png" sizes="96x96">
-<link rel="icon" type="image/png" href="lib/css/image/favicon/favicon-16x16.png" sizes="16x16">
-<link rel="manifest" href="lib/css/image/favicon/manifest.json">
+<meta content="lib/css/image/favicon/favicon-192x192.png" itemprop="image">
+<!-- <link rel="icon" type="image/png" href="lib/css/image/favicon/favicon-192x192.png" sizes="192x192"> -->
 <link rel="shortcut icon" href="lib/css/image/favicon/favicon.ico">
-<meta name="msapplication-TileColor" content="#B91D47">
-<meta name="msapplication-TileImage" content="lib/css/image/favicon/mstile-144x144.png">
-<meta name="msapplication-config" content="lib/css/image/favicon/browserconfig.xml">
-<!--<meta name="theme-color" content="#165B69">-->
+<!-- <meta name="theme-color" content="#165B69"> -->
 <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800">
 <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Pathway+Gothic+One">
 <link rel="stylesheet" type="text/css" href="lib/css/main.css">
 <link rel="stylesheet" type="text/css" href="lib/css/color.css">
-<!--<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">-->
+<!-- <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css"> -->
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 <script src="lib/js/main.js"></script>
 </head>
