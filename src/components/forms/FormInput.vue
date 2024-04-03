1efdfd2a7a9e2f2147a2fe3fb57db122e9f862e1
Jason Sims <jason@jasonsims.design>
2024-04-02 23:28:42 -0400

    Altered the FormInput component to fix broken v-model

--- src/components/forms/FormInput.vue
+++ src/components/forms/FormInput.vue
@@ -1,7 +1,14 @@
 <template>
   <label class="block text-sm font-semibold leading-6 text-white" v-if="label && name" :for="name">{{ label }}</label>
-  <component
-    :is="type === 'textarea' ? 'textarea' : 'input'"
+  <textarea
+    v-if="type === 'textarea'"
+    class="block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 backdrop-blur-md backdrop-filter focus:ring-2 focus:ring-inset focus:ring-cyan-500 sm:text-sm sm:leading-6"
+    v-model="model"
+    v-bind="$attrs"
+    :id="name"
+  />
+  <input
+    v-else
     class="block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 backdrop-blur-md backdrop-filter focus:ring-2 focus:ring-inset focus:ring-cyan-500 sm:text-sm sm:leading-6"
     v-model="model"
     v-bind="$attrs"
