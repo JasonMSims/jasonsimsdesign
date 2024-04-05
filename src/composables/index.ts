9bec8332826957d3354aea662a9e8f9f2115f4af
Jason Sims <jason@jasonsims.design>
2024-04-05 16:49:24 -0400

    refactor: :recycle: refactor composable functions and add index file

---
+++ src/composables/index.ts
@@ -0,0 +1,4 @@
+// @index('./*.{vue,js,ts}', f => `export { ${f.name} } from '${f.path}${f.ext}'`)
+export { useDateFormat } from './useDateFormat.ts'
+export { useScroll } from './useScroll.ts'
+// @endindex
