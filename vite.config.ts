a5e9ea866c6f08fafcdaa6789673733e014b06e1
Jason Sims <jason@jasonsims.design>
2024-04-05 16:51:17 -0400

    refactor: :recycle: add markdown-it and related files

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,22 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from 'url'
+import Markdown from 'unplugin-vue-markdown/vite'
+import { URL, fileURLToPath } from 'url'
+import { defineConfig } from 'vite'
 import svgLoader from 'vite-svg-loader'
+
+import markdownItSetup from './config/markdown-it'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), svgLoader()],
+  plugins: [
+    vue({
+      include: [/\.vue$/, /\.md$/],
+    }),
+    svgLoader(),
+    Markdown({
+      markdownItSetup,
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
