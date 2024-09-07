997efb0dbfe4145559f339d0f57d1459a1b983d7
JasonMSims <jason@jasonsims.design>
2024-09-07 13:42:26 -0400

    style: update footer

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -2,7 +2,7 @@
   <footer class="z-10 border-t border-t-zinc-200/5">
     <div class="mx-auto flex max-w-7xl flex-col items-center gap-4 px-4 py-8 sm:flex-row sm:justify-between sm:px-12">
       <RouterLink to="/"><JsdLogo class="h-16" /></RouterLink>
-      <p class="text-center text-xs text-zinc-500 sm:text-sm">&copy; {{ new Date().getFullYear() }} Jason Sims Design. All rights reserved.</p>
+      <p class="text-center text-xs text-zinc-500 sm:text-sm">&copy; {{ new Date().getFullYear() }} Jason Sims. All rights reserved.</p>
     </div>
   </footer>
 </template>
